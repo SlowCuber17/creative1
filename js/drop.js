3455a923bc67671e2c03980d32e6de519b173b57
EC2 Default User <ec2-user@ip-172-31-21-136.us-east-2.compute.internal>
2019-09-20 04:39:48 +0000

    Players added, Dice Roll updated, minor header changes

---
+++ js/drop.js
@@ -0,0 +1,11 @@
+function dropDown1() {
+  document.getElementById("drop1").classList.toggle("show");
+}
+
+function dropDown2() {
+  document.getElementById("drop2").classList.toggle("show");
+}
+
+function dropDown3() {
+  document.getElementById("drop3").classList.toggle("show");
+}
